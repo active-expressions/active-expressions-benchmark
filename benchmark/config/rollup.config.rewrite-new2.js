199ca4722e5208d0f54969c5b9729ce99dfb79c3
Stefan Lehmann <stefan.lehmann@hpi.de>
2017-08-29 08:29:19 +0200

    use includePath plugin to hardwire packages together

--- benchmark/config/rollup.config.rewrite-new2.js
+++ benchmark/config/rollup.config.rewrite-new2.js
@@ -1,23 +1,21 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import importAlias from 'rollup-plugin-import-alias';
+import includePaths from 'rollup-plugin-includepaths';
 
 export default {
     entry: 'benchmark/temp/rewriting-new/rewriting-new_babel_out.js',
     dest: 'benchmark/bundles/rewriting-new.js',
     plugins: [
+        includePaths({
+            include: {
+                ["aexpr-source-transformation-propagation"]: 'benchmark/temp/rewriting-new/aexpr-source-transformation-propagation/src/aexpr-source-transformation-propagation.js'
+            }
+        }),
         nodeResolve({
             jsnext: true,
             main: true
         }),
-        commonjs(),
-        importAlias({
-            Paths: {
-                ["aexpr-source-transformation-propagation"]: 'benchmark/temp/rewriting-new/aexpr-source-transformation-propagation', // hardlink with current version
-                Framework: './V1.0/Framework'
-            },
-            Extensions: ['js']
-        })
+        commonjs()
     ],
     format: 'iife'
 };
