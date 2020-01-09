8398315fd23ca71f7c9ba9f584b69e0f643d7e45
stlutz <dev@stlutz.net>
2020-01-09 19:59:02 +0100

    Refactor build script.

--- benchmark/build.js
+++ benchmark/build.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
 const nodeResolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const rollup = require('rollup');
@@ -8,83 +11,125 @@ const babel = require('babel-core');
 const srcDir = './src';
 const destDir = './build';
 
+// const program = new commander.Command();
+
+// program
+//   .option()
+
+
 function main() {
   buildAll();
 }
 
+// async function allBenchmarks() {
+//   try {
+//     const benchDirs = await fs.readdir(srcDir, { withFileTypes: true });
+//     benchDirs.forEach(benchDir => {
+//       if (!benchDir.isDirectory()) return;
+//       if (benchDir.name.startsWith('__')) return;
+      
+//       const files = await fs.readdir(path.join(srcDir, dirName))
+//     });
+//   } catch(err) {
+//     console.error(err);
+//   }
+// }
+
 async function buildAll() {
-  fs.readdir(srcDir, { withFileTypes: true }, (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-      if (!file.isDirectory()) return;
-      if (file.name.startsWith('__')) return;
-      const dir = file.name;
-      fs.emptydir(path.join(destDir, dir), err => {
-        if (err) return console.error(err);
-        fs.readdir(path.join(srcDir, dir), (err, files) => {
-          if (err) return console.error(err);
-          files.forEach(fileName => {
-            build(dir, fileName);
-          })
-        })
-      })
-    })
-  })
+  try {
+    const benchDirs = await fs.readdir(srcDir, { withFileTypes: true });
+    benchDirs.forEach(benchDir => {
+      if (!benchDir.isDirectory()) return;
+      if (benchDir.name.startsWith('_' || benchDir.name.startsWith('.'))) return;
+      
+      buildDirectory(benchDir.name);
+    });
+  } catch(err) {
+    console.error(err);
+  }
 }
 
-async function build(dir, file) {
-  const name = path.join(dir, file);
-  const type = path.basename(file, '.js');
-  const inFile = path.join(srcDir, dir, file);
-  const outFile = path.join(destDir, dir, file);
-  const tmpFile = path.join(srcDir, dir, `_${file}`);
-  
-  if (type === "rewriting" || type === "interpretation") {
-    let babelOpts = {};
+async function buildDirectory(dirName) {
+  try {
+    await fs.emptydir(path.join(destDir, dirName));
+    const files = await fs.readdir(path.join(srcDir, dirName));
+    
+    files.forEach(fileName => {
+      buildFile(dirName, fileName);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function buildFile(dirName, fileName) {
+  const name = path.join(dirName, fileName);
+  const type = path.basename(fileName, '.js');
+
+  const inFile = path.join(srcDir, dirName, fileName);
+  const outFile = path.join(destDir, dirName, fileName);
+  const tmpFile = path.join(srcDir, dirName, `_${fileName}`);
+
+  fs.copy(inFile, tmpFile);
+
+  try {
     if (type === "rewriting") {
-      babelOpts = {
-        plugins: ["babel-plugin-aexpr-source-transformation"],
-        presets: ["stage-0"],
-      }
+      await rewritingProcessing(tmpFile, tmpFile);
     } else if (type === "interpretation") {
-      babelOpts = {
-        plugins: ["babel-plugin-locals"],
-        presets: ["stage-0", ["es2015", { modules: false }]],
-      }
+      await interpretationProcessing(tmpFile, tmpFile);
     }
 
-    const source = await bundle(inFile, null, ['aexpr-source-transformation-propagation', 'aexpr-interpretation']);
-    const transformedSource = transpile(source, babelOpts);
-    
-    try {
-      await fs.outputFile(tmpFile, transformedSource);
-      await bundle(tmpFile, outFile);
-    } catch (err) {
-      console.error(err);
-    } finally {
-      await fs.remove(tmpFile);
-    }
-  } else {
-    await bundle(inFile, outFile);
+    const finalSource = await bundle(tmpFile);
+    await fs.outputFile(outFile, finalSource);
+    console.log(`Compiled ${name}`);
+  } catch (err) {
+    console.log(err);
+    return;
+  } finally {
+    await fs.remove(tmpFile);
   }
-  console.log(`Compiled ${name}`);
+}
+
+async function interpretationProcessing(inFile, outFile) {
+  const babelOpts =  {
+    plugins: ["babel-plugin-locals"],
+    presets: ["stage-0", ["es2015", { modules: false }]],
+  }
+
+  const source = await bundle(inFile, ['aexpr-interpretation']);
+  const transformedSource = transpile(source, babelOpts);
+  return fs.outputFile(outFile, transformedSource);
+}
+
+async function rewritingProcessing(inFile, outFile) {
+  const babelOpts =  {
+    plugins: ["babel-plugin-aexpr-source-transformation"],
+    presets: ["stage-0"],
+  }
+
+  const source = await bundle(inFile, ['aexpr-source-transformation-propagation']);
+  const transformedSource = transpile(source, babelOpts);
+  return fs.outputFile(outFile, transformedSource);
 }
 
 function transpile(code, optsOverride) {
   const opts = {
-    presets: [],
-    plugins: [],
-    // babelrc: false,
     ast: false,
     compact: false,
     sourceType: "script",
   };
   Object.assign(opts, optsOverride);
 
+  return noLogDuring(() => {
+    return babel.transform(code, opts).code
+  });
+}
+
+function noLogDuring(fn) {
   const originalLog = console.log;
   try {
     console.log = (...msgs) => {};
-    return babel.transform(code, opts).code;
+    return fn(originalLog);
   } catch (err) {
     console.error(err);
   } finally {
@@ -92,9 +137,10 @@ function transpile(code, optsOverride) {
   }
 }
 
-async function bundle(inFile, outFile, external) {
+async function bundle(inFile, external) {
   const bundle = await rollup.rollup({
     input: inFile,
+    inlineDynamicImports: true,
     external: [
       '../common.js', //can not be rollup-ed (without modifications); depends on `require.main`
       'is-ci',
@@ -108,15 +154,8 @@ async function bundle(inFile, outFile, external) {
     ],
   });
   
-  if (outFile) {
-    await bundle.write({
-      format: "cjs",
-      file: outFile,
-    });
-  } else {
-    const result = await bundle.generate({ format: 'cjs' });
-    return result.output[0].code;
-  }
+  const result = await bundle.generate({ format: 'cjs' });
+  return result.output[0].code;
 }
 
 main();
