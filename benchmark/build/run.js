092fd75862f51bb7aa2321402487e28767bea390
stlutz <dev@stlutz.net>
2020-01-09 20:15:29 +0100

    Add modified run script (original taken from nodejs).

--- benchmark/build/run.js
+++ benchmark/build/run.js
@@ -1,18 +1,30 @@
+#!/usr/bin/env node
 'use strict';
 
-const path = require('path');
 const fork = require('child_process').fork;
+const path = require('path');
 const CLI = require('./_cli.js');
 
+//
+// Parse arguments
+//
 const cli = CLI(`usage: ./node run.js [options] [--] <category> ...
   Run each benchmark in the <category> directory a single time, more than one
   <category> directory can be specified.
 
-  --filter pattern          string to filter benchmark scripts
+  --runs 1                  number of samples
+  --all                     run all benchmarks, ignoring the given categories
+                            (filter & exclude still applies)
+  --node ./node-binary      node binary to run the benchmarks with
+  --filter   pattern        includes only benchmark scripts matching <pattern>
+                            (can be repeated)
+  --exclude  pattern        excludes scripts matching <pattern> (can be
+                            repeated)
   --set    variable=value   set benchmark variable (can be repeated)
   --format [simple|csv]     optional value that specifies the output format
-`, { arrayArgs: ['set'] });
-const benchmarks = cli.benchmarks();
+`, { arrayArgs: ['set', 'filter', 'exclude'], boolArgs: ['all'] });
+
+const benchmarks = cli.optional.all ? cli.allBenchmarks() : cli.benchmarks();
 
 if (benchmarks.length === 0) {
   console.error('No benchmarks found');
@@ -32,46 +44,57 @@ if (format === 'csv') {
   console.log('"filename", "configuration", "rate", "time"');
 }
 
-(function recursive(i) {
+const runs = cli.optional.runs ? parseInt(cli.optional.runs, 10) : 1;
+
+(function runBenchmark(i) {
   const filename = benchmarks[i];
-  const child = fork(path.resolve(__dirname, filename), cli.optional.set);
+  (function runSample(j) {
+    const child = fork(path.resolve(__dirname, filename), cli.optional.set, {
+      execPath: cli.optional.node || process.execPath
+    });
 
-  if (format !== 'csv') {
-    console.log();
-    console.log(filename);
-  }
-
-  child.on('message', (data) => {
-    if (data.type !== 'report') {
-      return;
-    }
-    // Construct configuration string, " A=a, B=b, ..."
-    let conf = '';
-    for (const key of Object.keys(data.conf)) {
-      conf += ` ${key}=${JSON.stringify(data.conf[key])}`;
-    }
-    // Delete first space of the configuration
-    conf = conf.slice(1);
-    if (format === 'csv') {
-      // Escape quotes (") for correct csv formatting
-      conf = conf.replace(/"/g, '""');
-      console.log(`"${data.name}", "${conf}", ${data.rate}, ${data.time}`);
-    } else {
-      var rate = data.rate.toString().split('.');
-      rate[0] = rate[0].replace(/(\d)(?=(?:\d\d\d)+(?!\d))/g, '$1,');
-      rate = (rate[1] ? rate.join('.') : rate[0]);
-      console.log(`${data.name} ${conf}: ${rate}`);
-    }
-  });
-
-  child.once('close', (code) => {
-    if (code) {
-      process.exit(code);
+    if (format !== 'csv') {
+      console.log();
+      console.log(filename);
     }
 
-    // If there are more benchmarks execute the next
-    if (i + 1 < benchmarks.length) {
-      recursive(i + 1);
-    }
-  });
+    child.on('message', (data) => {
+      if (data.type !== 'report') {
+        return;
+      }
+
+      // Construct configuration string, " A=a, B=b, ..."
+      let conf = '';
+      for (const key of Object.keys(data.conf)) {
+        conf += ` ${key}=${JSON.stringify(data.conf[key])}`;
+      }
+      // Delete first space of the configuration
+      conf = conf.slice(1);
+
+      if (format === 'csv') {
+        // Escape quotes (") for correct csv formatting
+        conf = conf.replace(/"/g, '""');
+        console.log(`"${data.name}", "${conf}", ${data.rate}, ${data.time}`);
+      } else {
+        var rate = data.rate.toString().split('.');
+        rate[0] = rate[0].replace(/(\d)(?=(?:\d\d\d)+(?!\d))/g, '$1,');
+        rate = (rate[1] ? rate.join('.') : rate[0]);
+        console.log(`${data.name} ${conf}: ${rate}`);
+      }
+    });
+
+    child.once('close', (code) => {
+      if (code) {
+        process.exit(code);
+        j = Infinity;
+      }
+
+      // If there are more benchmarks execute the next
+      if (j + 1 < runs) {
+        runSample(j + 1);
+      } else if (i + 1 < benchmarks.length) {
+        runBenchmark(i + 1);
+      }
+    });
+  })(0);
 })(0);
